0e7dc08fdc1451fd626d7e25a948b0fdb18144fb
Sascha Baranji <72728762+Sascha-B23@users.noreply.github.com>
2021-10-14 10:39:20 +0200

    Update config.php

--- docker/var/www/app/config.php
+++ docker/var/www/app/config.php
@@ -2,3 +2,9 @@
 
 defined('ENABLE_URL_REWRITE') or define('ENABLE_URL_REWRITE', true);
 defined('LOG_DRIVER') or define('LOG_DRIVER', 'system');
+define('PLUGIN_INSTALLER', true);
+// Plugins directory
+define('PLUGINS_DIR', __DIR__.DIRECTORY_SEPARATOR.'plugins');
+
+// Plugins directory URL
+define('PLUGIN_API_URL', 'https://kanboard.org/plugins.json');
